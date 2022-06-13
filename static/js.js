a9d4b5b5e9cd53157f9cb06a841b53133c14c00b
MatheusValera <lrfp1000@.gmail.com>
2022-06-13 17:14:10 -0300

    refacto

--- static/js.js
+++ static/js.js
@@ -1,3 +1,4 @@
+let tableResult = []
 getData = async () =>{
   const data = JSON.stringify({k: 3})
   let result = ''
@@ -5,30 +6,20 @@ getData = async () =>{
     .then( async (response) => {
       result = await response.json()
   })
-  console.log(result)
+  table = result.matrix
+  showData()
 }
 
 showData = async () => {
-  const data = JSON.stringify({k: $('#k').val()})
-  let matrix = ''
-
-  console.log("F:")
-
-  await fetch('/knn', { headers: { 'Content-Type': 'application/json'}, method: 'POST', body: data })
-    .then( async (response) => {
-      matrix = await response.json()
-  })
-
-
   let table = document.createElement('table')
   let row = document.createElement('tr')
-  for(let x=0; x<7; x++){
+  for(let x=0; x<tableResult.length; x++){
 
     row.append(document.createElement(th).html('X'+(x+1)))
   }
   table.append(row)
 
-  matrix.forEach(elem => {
+  tableResult.forEach(elem => {
     let row = document.createElement('tr')
     row.append(document.createElement(td).html(e))
     elem.forEach(e => {
